4322cdf5e1feb17dd0550ce6eceeff60447a610d
IshtiaqueNafis <nafu22@gmail.com>
2021-11-10 14:41:18 -0500

    added basic codes for function Composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+//region  basic functional
+function sayHello() {
+    return 'Hello world';
+}
+
+let fn = sayHello; // fn is getting the reference of the function say hello wont be called here
+
+console.log(fn()); // this will return hello world.
+//endregion
+
+
+//region Passing function as arguments
+function greet(fnMessage) {
+    console.log(fnMessage());
+}
+greet(sayHello)  //this will return hello world.
+//endregion
+
+//region anonymous function return 
+function sayhello2() {
+    return function () {
+        return 'Hello world';
+    }
+}
+let fn2 = sayhello2()
+console.log(fn2()) // Hello world will be returned with in a function
+
+//endregion
+
