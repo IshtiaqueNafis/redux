d4b65675fb52716eadfd9e5f4e0915beef7a3835
IshtiaqueNafis <nafu22@gmail.com>
2021-11-10 19:43:57 -0500

    installed react devToolsEnhancer for redux

--- src/Redux/actionTypes/actions.js
+++ src/Redux/actionTypes/actions.js
@@ -1,22 +1,28 @@
 import * as actions from "./actionTypes";
 
-export const bugAdded = description => ({
-    type: actions.BUG_ADDED,
-    payload: {
-        description
-    }
-});
+export const bugAdded = description => {
+    return ({
+        type: actions.BUG_ADDED,
+        payload: {
+            description
+        }
+    });
+};
 
-export const bugRemoved = id => ({
-    type: actions.BUG_REMOVED,
-    payload: {
-        id
-    }
-});
+export const bugRemoved = id => {
+    return ({
+        type: actions.BUG_REMOVED,
+        payload: {
+            id
+        }
+    });
+};
 
-export const bugResolved = id => (
-    {
-        type: actions.BUG_RESOLVED,
-        payload: {id}
-    }
-)
+export const bugResolved = id => {
+    return (
+        {
+            type: actions.BUG_RESOLVED,
+            payload: {id}
+        }
+    );
+}
